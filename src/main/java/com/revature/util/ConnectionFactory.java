cbaef6d33af7070e9b384320cc85b14ed87979fb
Braylen Strain <76047181+braylenstrain@users.noreply.github.com>
2022-06-02 03:58:52 -0600

    Implement ConnectionFactory

--- src/main/java/com/revature/util/ConnectionFactory.java
+++ src/main/java/com/revature/util/ConnectionFactory.java
@@ -1,5 +1,21 @@
 package com.revature.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
+	
+	//TODO change to env variables in ec2
+	private static final String URL = System.getenv("db_url");
+	private static final String USERNAME = System.getenv("db_username");
+	private static final String PASSWORD = System.getenv("db_password");
+	
+	private ConnectionFactory() {}
+	
+	//Establishes a connection to the database referenced by URL
+	public static Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(URL,USERNAME,PASSWORD);
+	}
 
 }
