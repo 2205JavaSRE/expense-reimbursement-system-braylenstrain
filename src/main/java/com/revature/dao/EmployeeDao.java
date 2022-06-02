ffad6b2a4f12ed21abfa0b435fbf7be81d7e3cb4
Braylen Strain <76047181+braylenstrain@users.noreply.github.com>
2022-06-02 03:58:41 -0600

    Implement Daos

--- src/main/java/com/revature/dao/EmployeeDao.java
+++ src/main/java/com/revature/dao/EmployeeDao.java
@@ -1,5 +1,29 @@
 package com.revature.dao;
 
-public class EmployeeDao {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import com.revature.models.Employee;
+import com.revature.util.ConnectionFactory;
+
+public class EmployeeDao {
+	
+	private EmployeeDao() {}
+
+	public static Employee selectEmployeeByUserPass(String username, String password) throws SQLException {
+		String sqlSelect = "SELECT * FROM employees WHERE username = ? AND password = ?";
+		
+		try (PreparedStatement ps = ConnectionFactory.getConnection().prepareStatement(sqlSelect)) {
+			ps.setString(1, username);
+			ps.setString(2, password);
+			
+			//Put results  into ResultSet and grab first/only result
+			ResultSet rs = ps.executeQuery();
+			rs.next();
+		
+			return new Employee(rs.getInt("id"), rs.getString("first_name"), rs.getString("last_name"), rs.getBoolean("finance_manager"));
+		}
+	}
+	
 }
--- src/main/java/com/revature/dao/ReimbursementsLogDao.java
+++ src/main/java/com/revature/dao/ReimbursementsLogDao.java
@@ -1,5 +1,59 @@
 package com.revature.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.revature.util.ConnectionFactory;
+
 public class ReimbursementsLogDao {
 
+	private ReimbursementsLogDao() {};
+	//TODO fix details
+//	public static void insertLog(Account account, String logDetails) throws SQLException {
+//		String sqlInsert = "INSERT INTO transactions_log VALUES(?, DEFAULT, ?)";
+//		
+//		try (PreparedStatement ps = ConnectionFactory.getConnection().prepareStatement(sqlInsert)) {
+//			ps.setInt(1, account.getAccountNumber());
+//			ps.setString(2, logDetails);
+//			
+//			ps.execute();
+//		}
+//	}
+	
+	//TODO fix details, what output needs to be returned?
+//	public static StringBuilder selectLogsByAccount(int accountNumber) throws SQLException {
+//		StringBuilder logsByAccount = new StringBuilder();
+//		
+//		String sqlSelect = "SELECT * FROM transactions_log WHERE account_number = ?";
+//		
+//		try (PreparedStatement ps = ConnectionFactory.getConnection().prepareStatement(sqlSelect)) {
+//			ps.setInt(1, accountNumber);
+//			
+//			//Put results  into ResultSet and grab first/only result
+//			ResultSet rs = ps.executeQuery();
+//			while(rs.next()) {
+//				logsByAccount.append(rs.getTimestamp("time_of_transaction") + " " + rs.getString("transaction_details") + "\n") ;
+//			}
+//			
+//			return logsByAccount;
+//		}
+//	}
+	//TODO fix details, what output needs to be returned?
+//	public static StringBuilder selectAllLogs() throws SQLException {
+//		StringBuilder logs = new StringBuilder();
+//		
+//		String sqlSelect = "SELECT * FROM transactions_log";
+//		
+//		try (PreparedStatement ps = ConnectionFactory.getConnection().prepareStatement(sqlSelect)) {
+//			
+//			//Put results  into ResultSet and grab first/only result
+//			ResultSet rs = ps.executeQuery();
+//			while(rs.next()) {
+//				logs.append(rs.getTimestamp("time_of_transaction") + " " + rs.getString("transaction_details") + "\n");
+//			}
+//			
+//			return logs;
+//		}
+//	}
 }
