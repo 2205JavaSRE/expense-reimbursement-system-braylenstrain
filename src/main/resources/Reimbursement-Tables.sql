2ff2e8eaf83f20b6099a96323c364eb8b4603271
Braylen Strain <76047181+braylenstrain@users.noreply.github.com>
2022-06-05 05:24:49 -0600

    Update Reimbursement-Tables.sql

--- src/main/resources/Reimbursement-Tables.sql
+++ src/main/resources/Reimbursement-Tables.sql
@@ -8,14 +8,15 @@ CREATE TABLE employees(
 );
 
 CREATE TYPE category AS ENUM ('LODGING', 'TRAVEL', 'FOOD', 'OTHER');
-CREATE TYPE approval AS ENUM ('PENDING', 'APPROVED', 'DENIED');
+CREATE TYPE status AS ENUM ('PENDING', 'APPROVED', 'DENIED');
 CREATE TABLE reimbursements_log(
+	id serial PRIMARY KEY,
+	time_of_action timestamp DEFAULT current_timestamp,
+	action_details text NOT NULL,
 	employee_id int NOT NULL,
 	employee_first_name text NOT NULL,
 	employee_last_name text NOT NULL,
-	time_of_action timestamp DEFAULT current_timestamp,
 	reimbursement_category category NOT NULL,
 	amount NUMERIC NOT NULL CHECK(amount > 0),
-	action_details text NOT NULL,
-	reimbursement_approval approval NOT NULL
+	reimbursement_status status NOT NULL
 );
