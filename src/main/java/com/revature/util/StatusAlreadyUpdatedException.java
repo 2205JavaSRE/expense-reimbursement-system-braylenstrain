79718062b2f749d4101bf93a0cdee6500274436a
Braylen Strain <76047181+braylenstrain@users.noreply.github.com>
2022-06-06 00:41:26 -0600

    Create StatusAlreadyUpdatedException.java

---
+++ src/main/java/com/revature/util/StatusAlreadyUpdatedException.java
@@ -0,0 +1,31 @@
+package com.revature.util;
+
+@SuppressWarnings("serial")
+public class StatusAlreadyUpdatedException extends Exception {
+
+	public StatusAlreadyUpdatedException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public StatusAlreadyUpdatedException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public StatusAlreadyUpdatedException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public StatusAlreadyUpdatedException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public StatusAlreadyUpdatedException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
